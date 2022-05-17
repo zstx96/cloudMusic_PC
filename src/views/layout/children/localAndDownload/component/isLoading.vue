6d54953d3251fbc7aa73143979aab6d9320af177
zs <2026214125@qq.com>
2022-05-17 10:11:17 +0800

    🎉 init(download):

---
+++ src/views/layout/children/localAndDownload/component/isLoading.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div(class="")
+    p 正在下载
+    div(v-if="downloadStore.downloadList.length")
+        template(v-for="item in downloadStore.downloadList" :key="item.id" class="flex gap-2")
+            span {{item.name}}
+            el-progress( :percentage="item.percentage")  
+            p {{item.percentage}}
+    el-empty(v-else)
+</template>
+
+<script lang="ts" setup>
+import { useDownloadStore } from '@/store/downloadStore'
+
+const downloadStore = useDownloadStore()
+</script>
+
+<style scoped lang="less"></style>
