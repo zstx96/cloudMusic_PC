c67c6e95cc424ffc5b4fefa7957115d67453d1ea
zs <2026214125@qq.com>
2022-05-17 15:39:27 +0800

    ...

---
+++ src/views/layout/children/localAndDownload/LocalAndDownload.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+div
+    p 下载管理
+    el-tabs(v-model="activeName")
+        el-tab-pane( label="已下载" name="downloaded")
+        el-tab-pane( label="正在下载" name="isLoading")
+        el-tab-pane( label="test" name="test")
+    component(:is="curComponent" )
+</template>
+
+<script lang="ts" setup>
+import { computed, defineAsyncComponent, ref } from 'vue'
+
+const activeName = ref('downloaded')
+
+const components = import.meta.glob('./component/*.vue')
+
+const curComponent = computed(() => {
+	const key = `./component/${activeName.value}.vue`
+	return defineAsyncComponent(components[key])
+})
+</script>
+
+<style scoped lang="less"></style>
