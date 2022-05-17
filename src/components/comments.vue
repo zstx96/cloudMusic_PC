514cdd79a820deccc5fe96ff060f104f187b7e75
zs <2026214125@qq.com>
2022-05-17 16:00:14 +0800

    🐞 fix(comment):
    不知道为什么，comment 消失不见了。。

---
+++ src/components/comments.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div(v-for="comment in comments" class="flex gap-3 py-3 ")
+    div(class="")
+        el-avatar(:src="comment.user.avatarUrl" class=" cursor-pointer" @click="$router.push({name:'user',params:{id:comment.user.userId}})" )
+    div(class="border-b  pb-3  flex-1 ")
+        div
+            span(v-text="comment.user.nickname" class="text-blue-500")
+            |:{{ comment.content }}
+        div(v-if="comment.beReplied" class=" bg-app-gray bg-opacity-30 p-2 gap-1 cursor-pointer")
+            div(v-for="item in comment.beReplied")
+                span(class=" text-blue-500" @click="$router.push({name:'user',params:{id:item.user.userId}})") @{{item.user.nickname}}:
+                span() {{comment.beReplied[0].content}}
+        div(class="flex justify-between my-2")
+            span(class=" text-app-gray") {{ dayjs(comment.timeStr).format('YYYY[年]MM[月]DD[日] hh:mm') }}
+            div(class="flex items-center gap-2")
+                span 举报
+                | |
+                thumb-up-button(:width="20" :height="20" :liked="comment.liked" @thumb-up="thumbUpComment(comment.commentId,1)" @not-thumb-up="") 
+                span {{ comment.likedCount }}
+                | |
+                el-icon()
+                    el-icon-share 
+                | |
+               
+</template>
+
+<script lang="ts" setup>
+import type { Comment } from '@/interface'
+import dayjs from 'dayjs'
+import thumbUpButton from '@/components/iconButton/thumbUpButton.vue'
+import { likeComment } from '@/api/song'
+
+defineProps<{ comments: Comment[] }>()
+
+// 如果超过规定时间还没有response,终止此次axios
+
+const thumbUpComment = async (id: number, t: number | undefined, type = 0) => {
+	likeComment(id, t, type).then((res) => {
+		// TODO like
+	})
+}
+</script>
+
+<style scoped lang="less"></style>
