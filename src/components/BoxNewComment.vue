333fb52ea5816ce5196d0e7b1f5a60107df4e675
zs <2026214125@qq.com>
2022-05-18 10:22:09 +0800

    ✨ feat(歌曲页面): 添加评论编写box

---
+++ src/components/BoxNewComment.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+el-dialog(
+    v-model="visible"
+    :before-close="handleClose"
+)
+    template(#title)
+        p(class=" text-center") 
+            span 当前歌曲:
+            span(class="font-normal text-xl") {{title}}
+    el-input(
+        v-model="text" 
+        type="textarea" 
+        :rows="5"
+        placeholder="发表评论"
+    )
+    p(class=" font-normal text-xl") ☺ @ #
+    .text-right 
+        el-button(type="primary") 评论
+
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+defineProps<{ visible: boolean; title: string }>()
+const emit = defineEmits<{
+	(event: 'update:visible', arg1: boolean): void
+}>()
+
+const handleClose = () => {
+	emit('update:visible', false)
+}
+
+const text = ref('')
+</script>
+
+<style scoped lang="less"></style>
--- src/views/layout/children/discovery/children/newSongs/newSongs.vue
+++ src/views/layout/children/discovery/children/newSongs/newSongs.vue
@@ -1,7 +1,31 @@
 <template lang="pug">
-p 最新音乐
+div
+    p 最新音乐
+    div(class="flex w-60 rounded-full overflow-hidden border text-center m-auto relative  ")
+        div(class="absolute  bg-[#bbbbbb] rounded-full w-1/2 h-full overflow-hidden transform transition opacity-50" 
+            :class="[(activeTab === '新碟上架')&& ' translate-x-full']"
+         )
+        div(
+            class="flex-1 py-1 cursor-pointer"
+            v-for="item in tabs"
+            v-text="item"
+            @click="activeTab = item"
+        )  
+    component(:is="NewSongsComp")
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import NewSongsComp from './component/NewSongsComp.vue'
 
-<style scoped lang="less"></style>
+const tabs: ('新歌速递' | '新碟上架')[] = ['新歌速递', '新碟上架']
+const activeTab = ref<'新歌速递' | '新碟上架'>('新歌速递')
+</script>
+
+<style scoped lang="less">
+.active-tab {
+	border-radius: 100px;
+	overflow: hidden;
+	background-color: #bbbbbb;
+}
+</style>
