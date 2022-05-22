54ba37b93e18cacac97cff86e03e274998dd5f82
zs <2026214125@qq.com>
2022-05-22 17:51:03 +0800

    🎈 perf(treeShaking): element-plus
    if use dark mode,dark css need to import manually

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-
 import router from '@/router'
 import { createPinia } from 'pinia'
 import { globalProperties } from './plugin/globalProperties'
 const app = createApp(App)
 
 const pinia = createPinia()
-app.use(ElementPlus).use(router).use(pinia).use(globalProperties)
+app.use(router).use(pinia).use(globalProperties)
 
 app.mount('#app')
